71a52b6ad543c67a9885648f825af61381a376ed
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-12-02 20:46:07 -0500

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
-import react from "react";
+import React from "react";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignUp from "./pages/signup/signUp"
 // import logo from './logo.svg';
 import Navbar from "./components/Navbar/Navbar"
+
 
 function App() {
   return (
