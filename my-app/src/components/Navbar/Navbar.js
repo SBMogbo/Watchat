13740ad52d23677bd65b8451c9e61e834b06d106
Manny Lou <mavkio1@gmail.com>
2020-12-02 21:51:09 -0500

    Fixed for webpage.

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -7,15 +7,14 @@ import SearchIcon from "@material-ui/icons/Search";
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import FeaturedVideoRoundedIcon from '@material-ui/icons/FeaturedVideo';
-import QueueRoundedIcon from '@material-ui/icons/Queue';
+import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { IconButton } from "@material-ui/core";
+import {IconButton} from "@material-ui/core";
 import "./style.css";
 
 
 function Navbar() {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -32,13 +31,13 @@ function Navbar() {
 
           {/*Header Left*/}
 
-          <div className="col-12 col-sm-3 d-flex justify-content-center p-1">
+          <div className="col-12 col-md-3 d-flex justify-content-center p-1">
             <img src="https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico"></img>
 
 
           </div>
           {/* Header Center */}
-          <div className={'col-12 col-sm-6 d-flex p-1 '}>
+          <div className={'col-12 col-md-6 d-flex p-1 '}>
             <div className="col-4 d-flex justify-content-center header_options">
               <HomeIcon fontSize="large"/>
             </div>
@@ -52,32 +51,31 @@ function Navbar() {
 
           {/* Start of Header Right*/}
 
-          <div className="col-12 col-sm-3 d-flex justify-content-center p-1 header_input">
+          <div className="col-12 col-md-3 d-flex justify-content-center p-1">
 
-              <SearchIcon fontSize="large"/>
-              <input placeholder="Search Movies" type="text"/>
-
+            <SearchIcon fontSize="large"/>
+            <input className={'header_input'} placeholder="Search Movies" type="text"/>
 
 
             {/* This is for the Drop Down Far Right. */}
 
 
-              <Button className="expand-button" aria-controls="simple-menu" aria-haspopup="true"
-                      onClick={handleClick}>
-                <ExpandMoreIcon/>
-              </Button>
-              <Menu
-                  id="simple-menu"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleClose}>Support</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-              </Menu>
+            <Button className="expand-button" aria-controls="simple-menu" aria-haspopup="true"
+                    onClick={handleClick}>
+              <ExpandMoreIcon/>
+            </Button>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Support</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
 
 
           </div>
