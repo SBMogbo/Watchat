1f0d9aae1376c39e9b58b066128fb077e7743012
grrazzlle <cel47@miami.edu>
2020-12-07 22:29:04 -0500

    working on auth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const user = require("./user");
+const authRoutes = require("./auth");
 
 // Post routes
 router.use("/api/user", user);
+router.use("/api/auth", authRoutes);
 
 module.exports = router;
